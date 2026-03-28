4003a3450d55b2a8cbaa829fa39786dede84e4c2
Miguel2729 <miguelbc1407@gmail.com>
2026-03-28 14:23:12 -0300

    Atualizar o install.sh

--- install.sh
+++ install.sh
@@ -7,35 +7,37 @@ echo "========================================================"
 if command -v apt &> /dev/null; then
     echo "📦 Distro: Debian/Ubuntu/Mint"
     sudo apt update
-    sudo apt install -y git python3 python3-pip portaudio19-dev python3-dev
+    sudo apt install -y git python3 python3-pip portaudio19-dev python3-dev nano
     
 elif command -v pacman &> /dev/null; then
     echo "📦 Distro: Arch/Manjaro"
-    sudo pacman -Syu --noconfirm git python python-pip portaudio
+    sudo pacman -Syu --noconfirm git python python-pip portaudio nano
     
 elif command -v dnf &> /dev/null; then
     echo "📦 Distro: Fedora/RHEL/CentOS"
-    sudo dnf install -y git python3 python3-pip portaudio-devel python3-devel
+    sudo dnf install -y git python3 python3-pip portaudio-devel python3-devel nano
     
 elif command -v zypper &> /dev/null; then
     echo "📦 Distro: openSUSE"
-    sudo zypper install -y git python3 python3-pip portaudio-devel python3-devel
+    sudo zypper install -y git python3 python3-pip portaudio-devel python3-devel nano
     
 elif command -v apk &> /dev/null; then
     echo "📦 Distro: Alpine"
     sudo apk update
-    sudo apk add git python3 py3-pip portaudio-dev
+    sudo apk add git python3 py3-pip portaudio-dev nano
     
 elif command -v emerge &> /dev/null; then
     echo "📦 Distro: Gentoo"
     echo "⚠️  No Gentoo, você já sabe o que está fazendo!"
-    sudo emerge --ask dev-vcs/git dev-lang/python dev-python/pip media-libs/portaudio
+    sudo emerge dev-vcs/git dev-lang/python dev-python/pip media-libs/portaudio app-editors/nano
     
 else
     echo "❌ Gerenciador de pacotes não reconhecido!"
     echo "Instale manualmente: git, python3, pip e portaudio19-dev(opcional)"
     exit 1
 fi
+
+rm -f /usr/lib/python3.*/EXTERNALLY-MANAGED
 
 # Clonar ou atualizar pyOS
 if [ -d "pyOS" ]; then
